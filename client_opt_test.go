7f109b815f80c8dd37034e61e1375b880c7c24af
BRUHITsABunny <53124399+BRUHItsABunny@users.noreply.github.com>
2023-05-04 03:23:55 +0200

    initial commit

--- client_opt_test.go
+++ client_opt_test.go
@@ -1,7 +1,38 @@
 package gokhttp_cookiejar
 
-import "testing"
+import (
+	"context"
+	gokhttp "github.com/BRUHItsABunny/gOkHttp"
+	gokhttp_requests "github.com/BRUHItsABunny/gOkHttp/requests"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
 
 func TestNewJarOption(t *testing.T) {
+	jar, err := NewCookieJar(".cookies", "", nil)
+	require.NoError(t, err, "cookies.NewCookieJar: errored unexpectedly.")
 
+	// To load cookies from existing .cookies into jar instance
+	// err = jar.Load()
+	// require.NoError(t, err, "jar.Load: errored unexpectedly.")
+
+	hClient, err := gokhttp.NewHTTPClient(
+		NewJarOption(jar),
+		// gokhttp_client.NewProxyOption("http://127.0.0.1:8888"),
+	)
+	require.NoError(t, err, "NewHTTPClient: errored unexpectedly.")
+
+	for i := 0; i <= 1; i++ {
+		req, err := gokhttp_requests.MakeGETRequest(context.Background(), "https://github.com")
+		require.NoError(t, err, "requests.MakeGETRequest: errored unexpectedly.")
+
+		_, err = hClient.Do(req)
+		require.NoError(t, err, "hClient.Do: errored unexpectedly.")
+
+		time.Sleep(time.Duration(500) * time.Millisecond)
+	}
+
+	err = jar.Save()
+	require.NoError(t, err, "jar.Save: errored unexpectedly.")
 }
--- go.mod
+++ go.mod
@@ -4,11 +4,19 @@ go 1.19
 
 require (
 	github.com/BRUHItsABunny/crypto-utils v0.0.5
-	github.com/BRUHItsABunny/gOkHttp v0.2.9
+	github.com/BRUHItsABunny/gOkHttp v0.3.0
+	github.com/stretchr/testify v1.8.1
 	github.com/vmihailenco/msgpack/v5 v5.3.5
 )
 
 require (
 	github.com/OneOfOne/xxhash v1.2.8 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.10.0 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/text v0.7.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
